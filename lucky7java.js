2472a54fa9c096143a208afc0d2ef61608adf56b
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2021-05-08 08:58:23 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -143,7 +143,7 @@ button.addEventListener('click',function()
         }
   if(potValue<=30){
     
-      pass=prompt('The insurnce contracted is of value 45 units, do you need insurance y/n ,  ');
+      pass=prompt('The insurnce contracted is of value 20 Lakhs, do you need insurance y/n ,  ');
       if(pass=='y'){
     
               if (window.confirm('If you click "ok" you would be redirected  ')) 
